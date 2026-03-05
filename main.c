9461dc9cd64dbc5af2fab124af64f139ff7935bd
Simon Vilášek <simi.vilasek@gmail.com>
2026-03-05 12:06:54 +0100

    Update main.c

--- main.c
+++ main.c
@@ -88,3 +88,78 @@ int main(void) {
 
     return 0;
 }
+#include <stdio.h>
+
+#define ROK 2025
+#define MIN_ZUSTATEK 100
+#define MAX_ZUSTATEK 10000000
+#define MIN_VKLAD 100
+#define MAX_VKLAD 100000
+
+int main(void) {
+    int buffer;
+    float stav_uctu;
+    float mesicni_vklad;
+    float mesicni_urok;
+    float rocni_urok = 3.5;
+    float urok;
+    float konecny_urok;
+    float celkovy_urok = 0;
+    float dan;
+
+    int dny[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    do {
+        buffer = 0;
+        printf("Zadejte svuj zustatek v rozmezi %d - %d:", MIN_ZUSTATEK, MAX_ZUSTATEK);
+        scanf("%f", &stav_uctu);
+        while (getchar() != '\n') {
+            buffer++;
+        }
+        if (buffer != 0) {
+            printf("Byl zadan spatny vstup. Zadejte znovu.\n");
+        }
+    } while (buffer != 0 || stav_uctu > MAX_ZUSTATEK || stav_uctu < MIN_ZUSTATEK);
+
+    do {
+        buffer = 0;
+        printf("Zadejte, kolik chcete pravidelne mesicne vkladat v rozmezi %d - %d:", MIN_VKLAD, MAX_VKLAD);
+        scanf("%f", &mesicni_vklad);
+        while (getchar() != '\n') {
+            buffer++;
+        }
+        if (buffer != 0) {
+            printf("Byl zadan spatny vstup. Zadejte znovu.\n");
+        } else if (mesicni_vklad < MIN_VKLAD) {
+            printf("Zadejte vetsi pravidelny vklad.\n");
+        } else if (mesicni_vklad > MAX_VKLAD) {
+            printf("Zadejte nizsi pravidelny vklad.\n");
+        }
+    } while (buffer != 0 || mesicni_vklad < MIN_VKLAD || mesicni_vklad > MAX_VKLAD);
+
+    mesicni_urok = rocni_urok / 100 / 12;
+
+    printf("-----------------------------------------------------------------------------------------------\n");
+    printf("Rocni uroky jsou %.2f%%\n", rocni_urok);
+    printf("Stav uctu byl na pocatku roku %.2f.\n", stav_uctu);
+    printf("-----------------------------------------------------------------------------------------------\n");
+    printf("   datum         na pocatku mesice     vklad           urok          dan(15%%)    na konci mesice\n");
+
+    for (int i = 0; i < 12; i++) {
+        float zacatek = stav_uctu;
+        stav_uctu = stav_uctu + mesicni_vklad;
+        urok = stav_uctu * mesicni_urok;
+        dan = urok * 0.15;
+        konecny_urok = urok - dan;
+        stav_uctu = stav_uctu + konecny_urok;
+        celkovy_urok = celkovy_urok + konecny_urok;
+
+        printf("%2d.%2d.%d      %12.2f Kc  %10.2f Kc  %10.2f Kc  %10.2f Kc   %12.2f Kc\n", dny[i], i + 1, ROK, zacatek,
+               mesicni_vklad,
+               urok, dan, stav_uctu);
+    }
+    printf("-----------------------------------------------------------------------------------------------\n");
+    printf("Stav na konci roku 2025 je %.2f Kc. Uroky bylo za rok 2025 nasporeno %.2f Kc.\n", stav_uctu, celkovy_urok);
+    printf("-----------------------------------------------------------------------------------------------\n");
+    return 0;
+}
