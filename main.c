d9c116aabbd825d9247aff2789906d68072c680e
Simon Vilášek <simi.vilasek@gmail.com>
2026-03-06 06:27:15 +0100

    Add functions for account balance and monthly deposit

--- main.c
+++ main.c
@@ -149,4 +149,147 @@ int main(void) {
     printf("-----------------------------------------------------------------------------------------------\n");
 
     return 0; // konec programu
+} 
+#include <stdio.h>
+
+#define ROK 2025
+#define MIN_ZUSTATEK 100
+#define MAX_ZUSTATEK 10000000
+#define MIN_VKLAD 100
+#define MAX_VKLAD 100000
+
+// funkce pro načtení počátečního stavu účtu
+double nacti_zustatek() {
+
+    double stav_uctu;
+    int buffer;
+
+    do {
+        buffer = 0;
+
+        printf("Zadejte svuj zustatek v rozmezi %d - %d: ", MIN_ZUSTATEK, MAX_ZUSTATEK);
+        scanf("%lf", &stav_uctu);
+
+        while (getchar() != '\n') {
+            buffer++;
+        }
+
+        if (buffer != 0) {
+            printf("Byl zadan spatny vstup. Zadejte znovu.\n");
+        }
+
+    } while (buffer != 0 || stav_uctu > MAX_ZUSTATEK || stav_uctu < MIN_ZUSTATEK);
+
+    return stav_uctu;
+}
+
+
+// funkce pro načtení měsíčního vkladu
+double nacti_vklad() {
+
+    double mesicni_vklad;
+    int buffer;
+
+    do {
+        buffer = 0;
+
+        printf("Zadejte, kolik chcete pravidelne mesicne vkladat v rozmezi %d - %d: ", MIN_VKLAD, MAX_VKLAD);
+        scanf("%lf", &mesicni_vklad);
+
+        while (getchar() != '\n') {
+            buffer++;
+        }
+
+        if (buffer != 0) {
+            printf("Byl zadan spatny vstup. Zadejte znovu.\n");
+        }
+        else if (mesicni_vklad < MIN_VKLAD) {
+            printf("Zadejte vetsi pravidelny vklad.\n");
+        }
+        else if (mesicni_vklad > MAX_VKLAD) {
+            printf("Zadejte nizsi pravidelny vklad.\n");
+        }
+
+    } while (buffer != 0 || mesicni_vklad < MIN_VKLAD || mesicni_vklad > MAX_VKLAD);
+
+    return mesicni_vklad;
+}
+
+
+// funkce pro výpis hlavičky tabulky
+void vypis_hlavicku(double stav_uctu, double rocni_urok) {
+
+    printf("-----------------------------------------------------------------------------------------------\n");
+    printf("Rocni uroky jsou %.2lf%%\n", rocni_urok);
+    printf("Stav uctu byl na pocatku roku %.2lf Kc.\n", stav_uctu);
+    printf("-----------------------------------------------------------------------------------------------\n");
+
+    printf("   datum         na pocatku mesice     vklad           urok          dan(15%%)    na konci mesice\n");
+}
+
+
+// funkce pro výpočet
+void vypocet(double stav_uctu, double mesicni_vklad) {
+
+    double rocni_urok = 3.5;
+    double mesicni_urok = rocni_urok / 100 / 12;
+
+    double urok;
+    double konecny_urok;
+    double celkovy_urok = 0;
+    double dan;
+
+    int dny[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+    for (int i = 0; i < 12; i++) {
+
+        double zacatek = stav_uctu;
+
+        stav_uctu = stav_uctu + mesicni_vklad;
+
+        urok = stav_uctu * mesicni_urok;
+
+        dan = urok * 0.15;
+
+        konecny_urok = urok - dan;
+
+        stav_uctu = stav_uctu + konecny_urok;
+
+        celkovy_urok = celkovy_urok + konecny_urok;
+
+        printf("%2d.%2d.%d      %12.2lf Kc  %10.2lf Kc  %10.2lf Kc  %10.2lf Kc   %12.2lf Kc\n",
+               dny[i], i + 1, ROK,
+               zacatek,
+               mesicni_vklad,
+               urok,
+               dan,
+               stav_uctu);
+    }
+
+    printf("-----------------------------------------------------------------------------------------------\n");
+
+    printf("Stav na konci roku %d je %.2lf Kc. Uroky bylo za rok %d nasporeno %.2lf Kc.\n",
+           ROK,
+           stav_uctu,
+           ROK,
+           celkovy_urok);
+
+    printf("-----------------------------------------------------------------------------------------------\n");
+}
+
+
+// hlavní funkce
+int main() {
+
+    double stav_uctu;
+    double mesicni_vklad;
+
+    stav_uctu = nacti_zustatek();
+    mesicni_vklad = nacti_vklad();
+
+    vypis_hlavicku(stav_uctu, 3.5);
+
+    vypocet(stav_uctu, mesicni_vklad);
+
+    return 0;
 }
